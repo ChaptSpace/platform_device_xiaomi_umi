7f711b833c3084e487b57ca100a2a80da2db8dbb
chaptsand <chaptsand@gmail.com>
2022-09-07 23:30:33 +0800

    umi: hentaiOS-ify
    Change-Id: I23f282ec1b51144201ee70742ce741ab1948d38a

---
+++ hentai_umi.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+#
+# All components inherited here go to system image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+#
+# All components inherited here go to system_ext image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
+
+# All components inherited here go to product image
+$(call inherit-product, vendor/hentai/build/product/hentai_product.mk)
+
+#
+# All components inherited here go to vendor image
+#
+# TODO(b/136525499): move *_vendor.mk into the vendor makefile later
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
+
+# Inherit from the device configuration.
+$(call inherit-product, device/xiaomi/umi/device.mk)
+
+# Enforce dark boot animation
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.boot.theme=1
+
+PRODUCT_NAME := hentai_umi
+PRODUCT_DEVICE := umi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi 10
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
